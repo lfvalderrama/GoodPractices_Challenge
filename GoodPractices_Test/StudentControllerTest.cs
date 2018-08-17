0c2ab97c593a07bf7789b96f5e8ec32e7a30105a
Luis Valderrama <lfvalderramav@unal.edu.co>
2018-08-17 10:04:58 -0500

    Unit test 100% coverage Student Controller Test

--- GoodPractices_Test/StudentControllerTest.cs
+++ GoodPractices_Test/StudentControllerTest.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Data.Entity;
-using System.Linq;
-using GoodPractices_Controller;
+﻿using GoodPractices_Controller;
 using GoodPractices_Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
 
 namespace GoodPractices_Test
 {
@@ -14,10 +10,21 @@ namespace GoodPractices_Test
     public class StudentControllerTest
     {
 
+        private readonly List<Grade> dataGrade = new List<Grade>
+        {
+            new Grade {Subject = new Subject { Name = "Math 1" }, Period = "2018-1", Score = 4.2f, Type = GradeType.PARTIAL1 }
+        };
         private readonly List<Student> dataStudent = new List<Student>
             {
-                new Student { Name = "BBB", Document = "12341564" }
+                new Student { Name = "BBB", Document = "12341564", Grades = new List<Grade>()}
             };
+
+        private readonly List<ForeignLanguage> dataLanguage = new List<ForeignLanguage>
+            {
+                new ForeignLanguage { Name = "French 1"}
+            };
+
+        private readonly List<Course> dataCourse = new List<Course>();
 
         private Mock<ISchoolDBContext> _mockContext = new Mock<ISchoolDBContext>();
         private Mock<IValidation> _validator = new Mock<IValidation>();
@@ -83,25 +90,107 @@ namespace GoodPractices_Test
         }
         #endregion
 
-        //#region DeleteStudent_dont_saves_change
-        //[TestMethod]
-        //public void DeleteStudent_dont_saves_change()
-        //{
-        //    //Given
-        //    var mockSet = GeneralMock.GetQueryableMockDbSet(dataStudent);
+        #region DeleteStudent_dont_saves_change
+        [TestMethod]
+        public void DeleteStudent_dont_saves_change()
+        {
+            //Given
+            var mockSet = GeneralMock.GetQueryableMockDbSet(dataStudent);
+            _mockContext.Setup(c => c.Students).Returns(mockSet.Object);
+            _validator.Setup(v => v.CheckExistence(new Dictionary<string, string>() { { "student", "12341564" } })).Returns("The student identified by 12341564 doesn't exists");
+            _studentController = new StudentController(_mockContext.Object, _validator.Object);
 
-        //    var mockContext = new Mock<SchoolDBContext>();
-        //    mockContext.Setup(c => c.Students).Returns(mockSet.Object);
+            //When
+            var result = _studentController.DeleteStudent("12341564");
 
-        //    var controller = new StudentController(mockContext.Object);
+            //then
+            mockSet.Verify(m => m.Remove(It.IsAny<Student>()), Times.Never());
+            _mockContext.Verify(m => m.SaveChanges(), Times.Never());
+            Assert.AreEqual(result, "The student identified by 12341564 doesn't exists");
+        }
+        #endregion
 
-        //    //When
-        //    controller.DeleteStudent("1234156445");
+        #region AssignForeignLanguage_do_assign_the_language
+        [TestMethod]
+        public void ssignForeignLanguage_do_assign_the_language()
+        {
+            //Given
+            var mockSetStudent = GeneralMock.GetQueryableMockDbSet(dataStudent);
+            var mockSetLanguage = GeneralMock.GetQueryableMockDbSet(dataLanguage);
+            _mockContext.Setup(c => c.Students).Returns(mockSetStudent.Object);
+            _mockContext.Setup(f => f.ForeignLanguages).Returns(mockSetLanguage.Object);
+            _validator.Setup(v => v.CheckExistence(new Dictionary<string, string>() { { "student", "12341564" }, { "foreignLanguage", "French 1" } })).Returns("success");
+            _studentController = new StudentController(_mockContext.Object, _validator.Object);
+            //When
+            var result = _studentController.AssignForeignLanguage("12341564", "French 1");
 
-        //    //then
-        //    mockSet.Verify(m => m.Remove(It.IsAny<Student>()), Times.Never);
-        //    mockContext.Verify(m => m.SaveChanges(), Times.Never);
-        //}
-        //#endregion
+            //then
+            _mockContext.Verify(m => m.SaveChanges(), Times.Once());
+            Assert.AreEqual(result, "The foreign language French 1 was assigned satisfactorily to the student identified by 12341564");
+        }
+        #endregion
+
+        #region AssignForeignLanguage_don't_assign_the_language
+        [TestMethod]
+        public void ssignForeignLanguage_dont_assign_the_language()
+        {
+            //Given
+            var mockSetStudent = GeneralMock.GetQueryableMockDbSet(dataStudent);
+            var mockSetLanguage = GeneralMock.GetQueryableMockDbSet(dataLanguage);
+            _mockContext.Setup(c => c.Students).Returns(mockSetStudent.Object);
+            _mockContext.Setup(f => f.ForeignLanguages).Returns(mockSetLanguage.Object);
+            _validator.Setup(v => v.CheckExistence(new Dictionary<string, string>() { { "student", "12564" }, { "foreignLanguage", "French 1" } })).Returns("The student identified by 12564 doesn't exists");
+            _studentController = new StudentController(_mockContext.Object, _validator.Object);
+            //When
+            var result = _studentController.AssignForeignLanguage("12564", "French 1");
+
+            //then
+            _mockContext.Verify(m => m.SaveChanges(), Times.Never());
+            Assert.AreEqual(result, "The student identified by 12564 doesn't exists");
+        }
+        #endregion
+
+        #region GetGradeByPeriod_gets_grades
+        [TestMethod]
+        public void GetGradeByPeriod_gets_grades()
+        {
+            //Given
+            dataStudent[0].Grades.Add(dataGrade[0]);
+            var mockSetStudent = GeneralMock.GetQueryableMockDbSet(dataStudent);
+            var mockSetGrade = GeneralMock.GetQueryableMockDbSet(dataGrade);
+            _mockContext.Setup(c => c.Students).Returns(mockSetStudent.Object);
+            _mockContext.Setup(c => c.Grades).Returns(mockSetGrade.Object);
+            _validator.Setup(v => v.CheckExistence(new Dictionary<string, string>() { { "student", "12341564" } })).Returns("success");
+            _studentController = new StudentController(_mockContext.Object, _validator.Object);
+            //When
+            var result = _studentController.GetGradesByPeriod("12341564");
+
+            //then
+            var expected = new GradeReport { Name = "BBB", Grades = new Dictionary<string, List<GradesBy_>> { { "2018-1", new List<GradesBy_> { new GradesBy_ { Identifier = "Math 1", Grades = new List<Grade> { dataGrade[0] } } } } } };
+            Assert.AreEqual(expected.Name, result.Name);
+            Assert.AreEqual(expected.Grades.Count, result.Grades.Count);
+        }
+        #endregion
+
+        #region GetGradeByPeriod_gets_error
+        [TestMethod]
+        public void GetGradeByPeriod_gets_error()
+        {
+            //Given
+            dataStudent[0].Grades.Add(dataGrade[0]);
+            var mockSetStudent = GeneralMock.GetQueryableMockDbSet(dataStudent);
+            var mockSetGrade = GeneralMock.GetQueryableMockDbSet(dataGrade);
+            _mockContext.Setup(c => c.Students).Returns(mockSetStudent.Object);
+            _mockContext.Setup(c => c.Grades).Returns(mockSetGrade.Object);
+            _validator.Setup(v => v.CheckExistence(new Dictionary<string, string>() { { "student", "12341564" } })).Returns("The student identified by 12341564 doesnt exists");
+            _studentController = new StudentController(_mockContext.Object, _validator.Object);
+            //When
+            var result = _studentController.GetGradesByPeriod("12341564");
+
+            //then
+            Assert.AreEqual(result.Error, "The student identified by 12341564 doesnt exists");
+        }
+        #endregion
+
     }
 }
